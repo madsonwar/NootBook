4ea652e0d23a2edc2094d5b45924e44d3a0d9745
madson sousa <madsonwar21@gmail.com>
2016-01-27 14:07:41 -0300

    Classe Notbook, aribuos ligado, capacidade de processamento e programa
    foram criados no cpp e h classe NooBook.
    o metodo ligarCompuador.
    o metodo executarPrograna.

--- notebook/Notebook.cpp
+++ notebook/Notebook.cpp
@@ -9,4 +9,14 @@ Notebook::~Notebook()
 }
 
 void Notebook::ligarComputador(bool x){
+    if(x==1)
+        printf("Computador Ligado\n");
     }
+    
+void Nootbook::executarPrograma(String x){
+    
+        printf("Executando o programa %s\n",x);
+    
+    
+    }
+    
